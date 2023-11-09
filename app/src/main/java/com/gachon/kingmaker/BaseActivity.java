03597714d942fbf35b8be00dda71528d6a832406
ksj7385 <117991880+ksj7385@users.noreply.github.com>
2023-11-09 17:09:59 +0900

    drawer 요소들 추가

--- app/src/main/java/com/gachon/kingmaker/BaseActivity.java
+++ app/src/main/java/com/gachon/kingmaker/BaseActivity.java
@@ -1,6 +1,7 @@
 package com.gachon.kingmaker;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -9,17 +10,41 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import java.util.List;
 
 public abstract class BaseActivity extends AppCompatActivity {
+
+    protected DrawerLayout drawerLayout; // 모든 액티비티에서 사용될 드로어 레이아웃
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 드로어 레이아웃 초기화는 각 액티비티에서 setContentView 이후에 설정
+    }
+    protected void setupNavigationDrawer() {
+        // 드로어 아이콘 설정
+        ImageView drawerIcon = findViewById(R.id.drawerIcon);
+        if (drawerIcon != null) {
+            drawerIcon.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                }
+            });
+        }
+    }
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
+        // EditText 외부 터치 시 키보드 숨김 처리
         if (ev.getAction() == MotionEvent.ACTION_DOWN) {
             View v = getCurrentFocus();
             if (v instanceof EditText) {
@@ -28,7 +53,6 @@ public abstract class BaseActivity extends AppCompatActivity {
                 float x = ev.getRawX() + v.getLeft() - outLocation[0];
                 float y = ev.getRawY() + v.getTop() - outLocation[1];
 
-                // EditText가 포커스를 잃었을 때 키보드 숨김
                 if (x < v.getLeft() || x >= v.getRight() || y < v.getTop() || y > v.getBottom()) {
                     v.clearFocus();
                     InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -38,50 +62,89 @@ public abstract class BaseActivity extends AppCompatActivity {
         }
         return super.dispatchTouchEvent(ev);
     }
+
+    @Override
+    public void onBackPressed() {
+        // 드로어 레이아웃이 열려 있으면 닫기
+        if (drawerLayout != null && drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
     public class CustomAdapter extends ArrayAdapter<ListItem> {
         public CustomAdapter(Context context, List<ListItem> items) {
             super(context, 0, items);
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ListItem item = getItem(position);
-            if (item == null) return convertView;
+        public int getViewTypeCount() {
+            // 사용되는 뷰 타입의 총 개수 반환
+            return ListItem.ItemType.values().length;
+        }
 
+        @Override
+        public int getItemViewType(int position) {
+            // 각 포지션에 해당하는 아이템 타입 반환
+            return getItem(position).getItemType().ordinal();
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            int viewType = getItemViewType(position);
             ViewHolder holder;
 
+            // convertView 초기화 및 재사용 처리
             if (convertView == null) {
                 holder = new ViewHolder();
-                switch (item.getItemType()) {
-                    case RADIO_BUTTON:
+                switch (viewType) {
+                    case 0: // RADIO_BUTTON
                         convertView = LayoutInflater.from(getContext()).inflate(R.layout.radio_button_item, parent, false);
                         holder.radioButton = convertView.findViewById(R.id.radio_button);
-                        holder.radioButton.setText(item.getData());
                         break;
-                    case SLIDER:
+                    case 1: // SLIDER
                         convertView = LayoutInflater.from(getContext()).inflate(R.layout.slider_item, parent, false);
                         holder.slider = convertView.findViewById(R.id.slider);
-                        //holder.slider.setText(item.getData());
                         break;
-                    case OPTION_BUTTON:
+                    case 2: // OPTION_BUTTON
                         convertView = LayoutInflater.from(getContext()).inflate(R.layout.option_button_item, parent, false);
                         holder.optionButton = (Button) convertView.findViewById(R.id.option_button);
-                        holder.optionButton.setText(item.getData());
                         break;
-                    case TEXT_VIEW:
+                    case 3: // TEXT_VIEW
                         convertView = LayoutInflater.from(getContext()).inflate(R.layout.text_view_item, parent, false);
                         holder.textView = convertView.findViewById(R.id.text_view);
-                        holder.textView.setText(item.getData());
                         break;
+                    // 기타 뷰 타입에 따른 케이스들을 추가할 수 있습니다.
                 }
                 convertView.setTag(holder);
             } else {
                 holder = (ViewHolder) convertView.getTag();
             }
 
+            // 아이템 데이터에 따라 뷰 내용 설정
+            ListItem item = getItem(position);
+            switch (viewType) {
+                case 0: // RADIO_BUTTON
+                    holder.radioButton.setText(item.getData());
+                    break;
+                case 1: // SLIDER
+                    // 슬라이더 설정 (해당되는 경우)
+                    break;
+                case 2: // OPTION_BUTTON
+                    holder.optionButton.setText(item.getData());
+                    break;
+                case 3: // TEXT_VIEW
+                    holder.textView.setText(item.getData());
+                    break;
+                // 기타 뷰 타입에 따른 데이터 설정
+            }
+
             return convertView;
         }
-}
+
+
+    }
+    // 각 리스트 아이템의 뷰를 재사용하기 위한 뷰 홀더 클래스
     static class ViewHolder {
         RadioButton radioButton;
         SeekBar slider;
@@ -89,4 +152,3 @@ public abstract class BaseActivity extends AppCompatActivity {
         TextView textView;
     }
 }
-
--- app/src/main/java/com/gachon/kingmaker/HomeActivity.java
+++ app/src/main/java/com/gachon/kingmaker/HomeActivity.java
@@ -4,19 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class HomeActivity extends BaseActivity {
 
-    DrawerLayout drawerLayout;
     Button button;
     TextView goBackTextView;
 
@@ -25,73 +20,57 @@ public class HomeActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        drawerLayout = findViewById(R.id.drawer_layout);;
+        //drawer설정
+        setupNavigationDrawer();
+        // BaseActivity의 drawerLayout을 현재 레이아웃과 연결
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        // UI 요소 초기화
         button = findViewById(R.id.button);
         goBackTextView = findViewById(R.id.goBack_textView);
 
-        // 네비게이션 드로어 아이콘 찾기 및 클릭 이벤트 리스너 설정
-        ImageView drawerIcon = findViewById(R.id.drawerIcon);
-        drawerIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // 드로어 열기
-                drawerLayout.openDrawer(GravityCompat.START);
-            }
-        });
-
+        // 버튼 이벤트 리스너
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
-                startActivity(intent);
-
-                //onBackPressed();
+                startActivity(new Intent(HomeActivity.this, LoginActivity.class));
                 finish();
             }
         });
-
+        // '뒤로 가기' 텍스트 이벤트 리스너
         goBackTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
-                startActivity(intent);
-
-                //onBackPressed();
+                startActivity(new Intent(HomeActivity.this, LoginActivity.class));
                 finish();
             }
         });
 
+        // 리스트뷰 설정
         ListView listView = findViewById(R.id.menu_list);
-
-        // 예시 데이터 생성
         List<ListItem> items = Arrays.asList(
                 new ListItem(ListItem.ItemType.RADIO_BUTTON, "라디오 버튼"),
                 new ListItem(ListItem.ItemType.SLIDER, ""),
                 new ListItem(ListItem.ItemType.OPTION_BUTTON, "옵션 버튼"),
                 new ListItem(ListItem.ItemType.TEXT_VIEW, "텍스트 뷰"),
+
                 new ListItem(ListItem.ItemType.RADIO_BUTTON, "라디오 버튼2"),
                 new ListItem(ListItem.ItemType.SLIDER, ""),
                 new ListItem(ListItem.ItemType.OPTION_BUTTON, "옵션 버튼2"),
                 new ListItem(ListItem.ItemType.TEXT_VIEW, "텍스트 뷰2"),
+
                 new ListItem(ListItem.ItemType.RADIO_BUTTON, "라디오 버튼3"),
                 new ListItem(ListItem.ItemType.SLIDER, ""),
                 new ListItem(ListItem.ItemType.OPTION_BUTTON, "옵션 버튼3"),
-                new ListItem(ListItem.ItemType.TEXT_VIEW, "텍스트 뷰3")
+                new ListItem(ListItem.ItemType.TEXT_VIEW, "텍스트 뷰3"),
+
+                new ListItem(ListItem.ItemType.RADIO_BUTTON, "라디오 버튼4"),
+                new ListItem(ListItem.ItemType.SLIDER, ""),
+                new ListItem(ListItem.ItemType.OPTION_BUTTON, "옵션 버튼4"),
+                new ListItem(ListItem.ItemType.TEXT_VIEW, "텍스트 뷰4")
         );
 
-        // 어댑터 생성 및 리스트뷰에 설정
         CustomAdapter adapter = new CustomAdapter(this, items);
         listView.setAdapter(adapter);
-    }
-
-
-    @Override
-    public void onBackPressed() {
-        // 뒤로가기 버튼 클릭 시 드로어가 열려있다면 드로어를 닫음
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
     }
 }
